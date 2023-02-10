0deb55f4dfeef6ae87f4c804ac02e468e825328f
Philip Winsnes <philip.winsnes@icloud.com>
2023-02-10 17:34:19 +0100

    Added a component for adjusting the interval

--- src/App.js
+++ src/App.js
@@ -3,24 +3,54 @@ import confetti from 'canvas-confetti';
 import './App.css';
 
 function App() {
-  const [number, setNumber] = useState(0);
+	const [randomNumber, setRandomNumber] = useState(0);
+	const [interval, setInterval] = useState({ min: 0, max: 16 });
 
-  const handleClick = () => {
-    const randomNumber = Math.floor(Math.random() * 17);
-    setNumber(randomNumber);
-    confetti({
-      particleCount: 200,
-      spread: 360,
-      origin: { y: 0.45 },
-    });
-  };
+	const handleRandomize = () => {
+		setRandomNumber(
+			Math.floor(Math.random() * (interval.max - interval.min + 1)) +
+				interval.min
+		);
+		confetti({
+			particleCount: 200,
+			spread: 360,
+			origin: { y: 0.4 },
+		});
+	};
 
-  return (
-    <div className="App">
-      <h1 className="number" >{number}</h1>
-      <button className="button" onClick={handleClick}>Randomize</button>
-    </div>
-  );
+	const handleSetInterval = () => {
+		setInterval({
+			min: Number(document.getElementById('min').value),
+			max: Number(document.getElementById('max').value),
+		});
+	};
+
+	return (
+		<div className="App">
+			<div className="number">{randomNumber}</div>
+			<button onClick={handleRandomize} className="randomize-button">
+				Randomize
+			</button>
+			<div className="interval-container">
+				<label htmlFor="min">Min: </label>
+				<input
+					id="min"
+					type="number"
+					defaultValue={interval.min}
+					className="interval-input"
+          onChange={handleSetInterval}
+				/>
+				<label htmlFor="max">Max: </label>
+				<input
+					id="max"
+					type="number"
+					defaultValue={interval.max}
+					className="interval-input"
+          onChange={handleSetInterval}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
